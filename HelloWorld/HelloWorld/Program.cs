5cb1c56da4d3542a5136cb44998708b7542c8c12
Mattias-M <mattias.martensson@elev.ga.ntig.se>
2020-09-01 12:18:28 +0200

    age

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,45 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas!");
+            Console.WriteLine("Skriv ditt namn");
+
+            string namn = Console.ReadLine();
+
+            Console.WriteLine("Ditt namn är: " + namn);
+
+            Console.WriteLine("Din ålder:");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your age is: " + age);
+            Console.WriteLine("Lever du skriv ja, om du är död skriv nej");
+
+            bool alive = true;
+            var pulscheck = Console.ReadLine();
+
+            if (pulscheck == "ja")
+            {
+                alive = true;
+
+            }
+            else
+            {
+                alive = false;
+            }
+            if (alive == true)
+            {
+                Console.WriteLine("Ditt namn är " + namn);
+                Console.WriteLine("Din ålder är " + age);
+                Console.WriteLine("Du lever fortfarande");
+            }
+            else
+            {
+                Console.WriteLine("Ditt namn är" + namn);
+                Console.WriteLine("Din ålder är" + age);
+                Console.WriteLine("Du är DÖD");
+            }
+
+
         }
     }
 }

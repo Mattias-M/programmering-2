bbe0894e1d1b01d2a8a6622c4f87b716d5745461
Mattias-M <mattias.martensson@elev.ga.ntig.se>
2020-09-01 18:57:16 +0200

    hello

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,44 +6,62 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Skriv ditt namn");
-
-            string namn = Console.ReadLine();
-
-            Console.WriteLine("Ditt namn är: " + namn);
-
-            Console.WriteLine("Din ålder:");
-
-            int age = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Your age is: " + age);
-            Console.WriteLine("Lever du skriv ja, om du är död skriv nej");
-
-            bool alive = true;
-            var pulscheck = Console.ReadLine();
-
-            if (pulscheck == "ja")
+            while (true)
             {
-                alive = true;
+            
+                Console.WriteLine("Skriv ditt namn");
 
-            }
-            else
-            {
-                alive = false;
-            }
-            if (alive == true)
-            {
-                Console.WriteLine("Ditt namn är " + namn);
-                Console.WriteLine("Din ålder är " + age);
-                Console.WriteLine("Du lever fortfarande");
-            }
-            else
-            {
-                Console.WriteLine("Ditt namn är" + namn);
-                Console.WriteLine("Din ålder är" + age);
-                Console.WriteLine("Du är DÖD");
-            }
+                string namn = Console.ReadLine();
 
+                Console.WriteLine("Ditt namn är: " + namn);
+
+                Console.WriteLine("Din ålder:");
+
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                switch (age)
+                {
+                    case >18:
+                        Console.WriteLine("du är under 18");
+
+                    case Z
+
+                }
+
+                Console.WriteLine("Your age is: " + age);
+               
+                for(int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine("Lever du skriv ja, om du är död skriv nej");
+                }
+                Console.WriteLine("Lever du skriv ja, om du är död skriv nej");
+
+                bool alive = true;
+                var pulscheck = Console.ReadLine();
+
+                if (pulscheck == "ja")
+                {
+                    alive = true;
+
+                }
+                else
+                {
+                    alive = false;
+                }
+                if (alive == true)
+                {
+                    Console.WriteLine("Ditt namn är " + namn);
+                    Console.WriteLine("Din ålder är " + age);
+                    Console.WriteLine("Du lever fortfarande");
+                }
+                else
+                {
+                    Console.WriteLine("Ditt namn är" + namn);
+                    Console.WriteLine("Din ålder är" + age);
+                    Console.WriteLine("Du är DÖD");
+                    break;
+                }
+            }
 
         }
     }
